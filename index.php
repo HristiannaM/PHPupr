0522c8a95b441a65402cda63cddae300370aceed
HristiannaM <hrisi_razgrad@abv.bg>
2020-09-19 13:27:38 +0300

    update

--- index.php
+++ index.php
@@ -1,2 +1,34 @@
 <?php
-echo "hi";
+echo "Hi";
+$username = "Hristianna";
+echo "<br>";
+echo $username;
+$age = 22;
+echo "<br>";
+echo $age;
+$istrue = true;
+echo"<br>";
+echo $istrue;
+
+$array = ["Hristianna",22];
+echo"<br>";
+echo $array[0];
+echo"<br>";
+echo $array[1];
+
+$assocarray = ["username" => "Hristianna", "age" => 22];
+echo "<br>";
+echo $assocarray["username"];
+
+for($i=0;$i<2;$i++)
+ {
+    echo"<br>";
+    echo $array[$i];
+ 
+ }
+
+ foreach($assocarray as $elements)
+ {
+    echo"<br>";
+    echo $elements;
+ } 
